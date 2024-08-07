439b7affca7134fb7a00b56af50a4cf61bf7a8a5
Cha <ckwhdgus159@naver.com>
2024-08-07 14:28:28 +0900

    포트번호 변경

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
     @GetMapping("/")
     public String getMethodName() {
-        return "Hello";
+        return "된다구리!!";
     }
 }
